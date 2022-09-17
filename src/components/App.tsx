01ae88490df078a1593fdb0deffe2f22399281e4
Jose Antonio Castro <jacastro18@uc.cl>
2022-09-17 12:27:58 -0300

    feat: add python style, localStorage & ans counter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,26 +8,55 @@ import ReactDiffViewer from 'react-diff-viewer';
 import { useTestCases, useCodeRun } from '~/lib/CodeContext';
 import readmeExample from '~/assets/Readme.md?raw';
 import CodeMirror from '@uiw/react-codemirror';
+import { python } from "@codemirror/lang-python";
 import { ITestCaseStatus } from "../lib/types"
+import useLocalStorage from '../hooks/useLocalStorage';
 
 
 export default function App() {
   const testCases = useTestCases();
   const runCode = useCodeRun();
-  const [seeDiff, setSeeDiff] = useState(false);
-  const [code, setCode] = useState("# Escribe tú código aquí\n");
+  const starterCode = "# Escribe tú código aquí\n";
+  const [currentCode, storeCode] = useLocalStorage(starterCode);
+  const [code, setCode] = useState(currentCode || starterCode);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    storeCode(code);
+  }, [code]);
+
+  // Se actualiza mal el contador,
+  // Al tener todas buenas, volver a correr y tener malas no se actualiza bien
+  function addCounter(counter, total) {
+    if (counter < total) {
+      setCounter(counter + 1);
+    }
+  }
+
 
   return (
     <div className="grid grid-cols-2 h-screen max-h-screen">
       <ReactMarkdown className="prose h-full max-w-none p-10 overflow-y-scroll text-justify" children={readmeExample} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
       <div className="flex flex-col p-10 h-full overflow-y-scroll">
         <div>
-          <CodeMirror className="rounded-xl" value={code} onChange={(value) => setCode(value)} lang="py" />
-          <button type="button" className="btn mb-10" onClick={() => { runCode([{ name: 'main.py', content: code }]); setSeeDiff(true); }}>Run code</button>
+          <CodeMirror
+            className="rounded-xl text-gray-700"
+            value={code}
+            onChange={(value) => setCode(value)}
+            lang="py"
+            extensions={[python()]}
+          />
+          <button type="button" className="btn mb-10 mt-4" onClick={() => { setCounter(0); runCode([{ name: 'main.py', content: code }]) }}>Run code</button>
+        </div>
+        <div>
+          <p className='text-xl font-medium px-2 pb-4 cursor-pointer'>
+            Correct answers {counter} / {testCases.length}
+          </p>
         </div>
         <ul className="flex flex-col gap-2">
           {testCases.map((testCase) => (
             <li key={testCase.test.id} className="border border-base-300 bg-base-100 rounded-box">
+              {testCase?.result?.status === ITestCaseStatus.PASSED && addCounter(counter, testCases.length)}
               <details>
                 <summary className='text-xl font-medium p-4 cursor-pointer'>
                   {testCase.name}
@@ -63,8 +92,8 @@ export default function App() {
                       <div>
                         <p className="font-bold">Diff:</p>
                         <ReactDiffViewer
-                          oldValue={testCase.result.output.join('\n')}
-                          newValue={testCase.expectedOutput.join('\n')}
+                          newValue={testCase.result.output.join('\n')}
+                          oldValue={testCase.expectedOutput.join('\n')}
                           splitView
                         />
                       </div>
