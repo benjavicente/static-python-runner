9a1dfc76124a5a88341801437ac661d03145851f
Benjamín Vicente <benjavicente@uc.cl>
2022-09-19 12:03:27 -0300

    update the runner

--- src/lib/pythonRunner.tsx
+++ src/lib/pythonRunner.tsx
@@ -1,7 +1,8 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { createPythonInstructor, PythonInstructor } from "~/lib/pythonInstructor";
-
-export type ICodeContext = {};
+import { IFile } from "~/types/files";
+import { IRunnerOutput } from "~/types/python.worker";
+import { ITestCaseLoaded } from "~/types/testCases";
 
 const PythonRunnerContext = createContext<null | ICodeContext>(null);
 
@@ -25,32 +26,70 @@ function useWorkers(number: number) {
   return workers;
 }
 
+type Callback = (index: number, result: IRunnerOutput) => void;
+type QueueItem = [index: number, files: IFile[], test: ITestCaseLoaded, resolve: Callback];
+type RunnerFn = (testCases: ITestCaseLoaded[], files: IFile[], callback: Callback) => void;
+
+export type ICodeContext = {
+  runTestCases: RunnerFn;
+  threads: number;
+  setThreads: (number: number) => void;
+  isReady: boolean;
+  interrupt: () => void;
+  isRunning: boolean;
+};
 
 // TODO: ver como organizar los test cases y el queue de estos
 export default function PythonRunnerProvider({ ...props }) {
-  const [threads, setThreads] = useState(3);
+  const [threads, setThreads] = useState(2);
   const workers = useWorkers(threads);
-  const [queue, setQueue] = useState([]);
+  const [queue, setQueue] = useState<QueueItem[]>([]);
+  const isReady = workers.length === threads;
+  const [isRunning, setIsRunning] = useState(false)
 
   useEffect(() => {
-    if (workers.length === 0) return;
+    if (!isReady) return;
+    let localQueue = queue;
 
-    // copilot suggestion
-    const runNext = () => {
-      if (queue.length === 0) return;
-      const [test, resolve] = queue.shift();
-      const worker = workers.shift();
-      worker.runCode(test).then((result) => {
-        resolve(result);
-        workers.push(worker);
-        runNext();
+    const runNext = (worker: PythonInstructor) => {
+      if (localQueue.length === 0) return
+      const isLast = localQueue.length === 1;
+      const [index, files, test, resolve] = localQueue.shift() as QueueItem;
+      const { entrypoint, zipFiles, input } = test;
+      worker.runCode({ entrypoint, files, input, zipFiles }).then((result) => {
+        if (isLast) setIsRunning(false);
+        resolve(index, result);
+        runNext(worker);
       });
     };
 
-    return () => {
-      second;
-    };
-  }, [queue, workers]);
+    workers.forEach((worker) => runNext(worker));
 
-  return <PythonRunnerContext.Provider value={null} {...props} />;
+    return () => {
+      localQueue = []
+    };
+  }, [queue, workers, isReady, isRunning]);
+
+  const runTestCases = useCallback<RunnerFn>((testCases, files, callback) => {
+    setIsRunning(true)
+    setQueue(testCases.map((test, index) => [index, files, test, callback]));
+  }, []);
+
+
+  const interrupt = useCallback(() => {
+    setQueue([]);
+    workers.map((worker) => worker.interrupt());
+    setIsRunning(false)
+  }, [workers]);
+
+
+  return <PythonRunnerContext.Provider value={{ threads, runTestCases, setThreads, isReady, isRunning, interrupt }} {...props} />;
+}
+
+export function usePythonRunner() {
+  const context = useContext(PythonRunnerContext);
+  if (context === null) {
+    throw new Error("usePythonRunner must be used within a PythonRunnerProvider");
+  }
+  return context;
 }
